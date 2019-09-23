fb801255bcc2ccf83b9b4ef9ec29c84c8b1fd65f
MwSpace LLC <alex@mwspace.com>
2019-09-23 14:31:11 +0200

    Update privacyjs.js

--- privacyjs.js
+++ privacyjs.js
@@ -1,24 +1,85 @@
-const privacyjs = document.getElementById('privacyjs');
+/** @init Class */
+const privacyjs = {};
+privacyjs.v = '1.0';
+privacyjs.e = document.getElementById('privacyjs');
+privacyjs.l = console.log('%cPrivacyJs ' + privacyjs.v, 'background: #222; color: #bada55');
 
-console.log.bind(console, "%c", "background: #3F51B5;color:#FFF;padding:5px;border-radius: 5px;line-height: 26px;", "");
+/**
+ * @PrivacyJs
+ * @author Aleksandr Ivanovitch
+ */
+class PrivacyJs {
+    init() {
+        const body = document.querySelector('body');
+        var privacy = document.createElement('div');
+        var span = document.createElement('span');
+        var accept = document.createElement('a');
+        var info = document.createElement('a');
 
-if (privacyjs) {
+        privacy.style.position = 'fixed';
+        privacy.style.width = '100%';
+        privacy.style.backgroundColor = privacyjs.e.backgroundColor ? privacyjs.e.backgroundColor : 'rgb(238, 238, 238)';
+        privacy.style.margin = 0;
+        privacy.style.left = 0;
 
-} else {
-    console.log('ciao')
+        privacyjs.e.dataset.bottom ?
+            privacy.style.bottom = privacyjs.e.dataset.bottom
+            : null;
+
+        privacyjs.e.dataset.top ?
+            privacy.style.top = privacyjs.e.dataset.top
+            : null;
+
+        privacy.style.padding = '8px';
+        privacy.style.zIndex = 9999999;
+
+        privacyjs.e.dataset.textAlign ?
+            privacy.style.textAlign = privacyjs.e.dataset.textAlign
+            : privacy.style.textAlign = 'center';
+
+        span.style.color = privacyjs.e.color ? privacyjs.e.color : 'rgb(34,34,34)';
+        span.innerHTML = privacyjs.e.dataset.message;
+
+        privacy.append(span);
+
+        if (privacyjs.e.dataset.info) {
+            info.innerText = privacyjs.e.dataset.info;
+            info.style.margin = '4px';
+            info.href = privacyjs.e.dataset.link;
+            info.target = '_blank';
+            span.append(info);
+        }
+
+        if (privacyjs.e.dataset.accept) {
+            accept.innerText = privacyjs.e.dataset.accept;
+            accept.style.margin = '4px';
+            accept.href = '#!';
+            span.append(accept);
+            accept.onclick = () => {
+                localStorage.setItem('privacyjs', 1);
+                privacy.remove();
+            };
+
+            if (!localStorage.getItem('privacyjs')) {
+                body.append(privacy);
+            }
+        } else {
+            body.append(privacy);
+        }
+    }
 }
 
-const body = document.querySelector('body');
-var privacy = document.createElement('div');
-var span = document.createElement('span');
-privacy.style.position = 'fixed';
-privacy.style.width = '100%';
-privacy.style.backgroundColor = 'rgb(238, 238, 238)';
-privacy.style.margin = 'fixed';
-privacy.style.left = 'fixed';
-privacy.style.top = 'fixed';
-privacy.style.padding = 'fixed';
-privacy.style.zIndex = 9999999;
-privacy.style.textAlign = 'center';
-
-span.innerHTML = privacyjs.dataset.text;
+if (!privacyjs.e) {
+    console.log('element-id #privacyjs not found')
+} else if (!privacyjs.e.dataset) {
+    console.log('element\'s -dataset #privacyjs not found');
+} else if (!privacyjs.e.dataset.message) {
+    console.log('element\'s message-dataset #privacyjs not found');
+} else {
+    /**
+     * Contstructor work
+     * @type {PrivacyJs}
+     */
+    privacyjs.i = new PrivacyJs();
+    privacyjs.i.init();
+}
